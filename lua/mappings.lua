c29aba8bfb4f091f61d66c9d244513b219bbb728
ghaisanghaisan <110543042+ghaisanghaisan@users.noreply.github.com>
2024-12-22 11:37:44 +0700

    my config

---
+++ lua/mappings.lua
@@ -0,0 +1,16 @@
+require "nvchad.mappings"
+-- vim.cmd [[
+--   highlight Normal guibg=none
+--   highlight NonText guibg=none
+--   highlight Normal ctermbg=none
+--   highlight NonText ctermbg=none
+-- ]]
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+map({ "n", "i", "v" }, "<C-s>", "<Esc><cmd> w <cr>")
